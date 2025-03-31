1af36f17420480d550cd3ad56fb9dddeea5f8985
daniel arega <danielarega25@gmail.com>
2025-03-31 11:15:28 -0200

    axios and request

---
+++ netflix/src/utils/axios.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import axios from "axios"
+function axios() {
+  return (
+    <div>
+      const instance=axios.create({
+
+        baseURL="https://api.themoviedb.org/3"
+      })
+    </div>
+  )
+}
+
+export default axios
